415b45d9864a17440a012273f686ffc753517ab6
kalanak-ux <kalana.20234122@iit.ac.lk>
2026-01-18 21:40:17 +0530

    first project

---
+++ src/main/java/org/example/hhhhhhhhhhhhhhhhhhhhhhh/HhhhhhhhhhhhhhhhhhhhhhhApplication.java
@@ -0,0 +1,15 @@
+package org.example.hhhhhhhhhhhhhhhhhhhhhhh;
+
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhhhhhhhhhhhhhhhhhhhhhhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhhhhhhhhhhhhhhhhhhhhhhApplication.class, args);
+    }
+    //sssssssssssssssssssssssssssssssssssshgfjgfjgfjhgf
+
+}
---
+++ src/test/java/org/example/hhhhhhhhhhhhhhhhhhhhhhh/HhhhhhhhhhhhhhhhhhhhhhhApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hhhhhhhhhhhhhhhhhhhhhhh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhhhhhhhhhhhhhhhhhhhhhhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
